f1f4e650f0af50b94b80fe6fe859acdd730b96ba
Adam Hartman <adamhartman5@gmail.com>
2022-11-02 21:30:12 -0400

    Add assets and change title and favicon

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Baseball Pals</title>
+        <link rel="icon" href="/cutty-small.png" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col px-20">
