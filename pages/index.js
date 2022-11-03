6bb81e9f446c36f64c064ece3e97e88a8ee5115f
Adam Hartman <adamhartman5@gmail.com>
2022-11-02 21:15:32 -0400

    Re-add files to master

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col px-20">
+        <h1 className='flex text-white align-top justify-center text-5xl z-99'>UNDER CONSTRUCTION</h1>
+        <div className='flex -z-50'><Image src='/cottoncandy.gif' layout='fill' /></div>
+      </main>
+    </div>
+  )
+}
