1a2d277d5bcc21e092d602b7b02f146fe33d9d9b
Adam Hartman <adamhartman5@gmail.com>
2022-11-07 21:03:07 -0500

    create fapredictions page

---
+++ pages/fapredictions.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function fapredictions() {
+  return (
+    <div><iframe src="https://docs.google.com/forms/d/e/1FAIpQLScl9QAJzcsVyReJMT8-OGY0ajZ58JfN48SDCRB0asvNyqXZHw/viewform?embedded=true" width="100%" height="10212" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe></div>
+  )
+}
