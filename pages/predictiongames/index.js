86dd39781fd580e1fdca8412e0eef19d3035ec22
Adam Hartman <adamhartman5@gmail.com>
2022-11-09 23:37:30 -0500

    Add link to prediction game

--- pages/predictiongames/index.js
+++ pages/predictiongames/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
 
 const index = () => {
   return (
-    <div>index</div>
+    <MainContainer>
+      <LinkContainer>
+        <Link href='./fapredictions' passHref legacyBehavior>
+          <LinkTag>
+            Free Agency Prediction Game
+          </LinkTag>
+        </Link>
+      </LinkContainer>
+      </MainContainer>
   )
 }
 
 export default index
+
+const MainContainer = styled.div`
+  
+`
+
+const LinkContainer = styled.div`
+
+`
+
+const LinkTag = styled.a`
+
+`
