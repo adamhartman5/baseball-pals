297dc0228bf6bd6e4261bc365ed5fee07957021b
Adam Hartman <adamhartman5@gmail.com>
2022-11-09 23:07:09 -0500

    Fix sidebar image

--- components/sidebar.js
+++ components/sidebar.js
@@ -31,12 +31,12 @@ export default sidebar
 
 const SidebarContainer = styled.div`
   width: 175px;
-  height: 100%;
+  height: 100vh;
   border-right: 1px solid gray;
   display: flex;
   background-image: url(${props => props.image.src});
   background-repeat: no-repeat;
-  background-size: cover;
+  background-size: contain;
   overflow-y: hidden;
 `
 
