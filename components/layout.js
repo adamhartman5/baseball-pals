f5f9cf766ee37d4f241c9e718e299c1000cf3348
Adam Hartman <adamhartman5@gmail.com>
2022-11-11 18:30:07 -0500

    Change background to navy

--- components/layout.js
+++ components/layout.js
@@ -25,6 +25,7 @@ export default layout
 const LayoutContainer = styled.div`
     display: flex;
     flex-direction: column;
+    background-color: navy;
     height: 100vh;
     margin: 0;
     padding: 0;
